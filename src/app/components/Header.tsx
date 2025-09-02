cfb623c630f92ce7764d326d44c5ab481d656e1c
brunoverpet <bverpet46@gmail.com>
2025-09-02 21:47:46 +0200

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="flex justify-end w-full">
+    <div className="flex justify-end">
       <div className="text-title text-5xl font-test">Streamly</div>
     </div>
   )
