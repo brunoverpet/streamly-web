dc46649ae9cb9f8ad3a4d8f08def4cb2045bb061
brunoverpet <bverpet46@gmail.com>
2025-09-03 22:56:11 +0200

    feat : update input components for search bar

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,14 +1,24 @@
+import { Search } from 'lucide-react'
+
 interface InputProps {
-  label: string
+  label?: string
   name: string
   placeholder: string
   type: 'text' | 'email' | 'password'
+  withSearchIcon?: boolean
 }
-export default function Input({ label, name, placeholder, type }: InputProps) {
+export default function Input({
+  label,
+  name,
+  placeholder,
+  type,
+  withSearchIcon = false,
+}: InputProps) {
   return (
     <div className="text-paragraph">
       <span>{label}</span>
-      <div className="border-paragraph border-1 rounded-sm p-2">
+      <div className="flex items-center gap-x-1 border-paragraph border-1 rounded-sm p-2">
+        {withSearchIcon && <Search className="text-title" />}
         <input
           id={name}
           name={name}
--- src/app/historique/page.tsx
+++ src/app/historique/page.tsx
@@ -1,12 +1,37 @@
+'use client'
+
 import Navbar from '@/app/components/Navbar'
 import ItemCard from '@/app/components/ItemCard'
+import { MoveLeft, Search } from 'lucide-react'
+import { useState } from 'react'
+import Input from '@/app/components/Input'
 
 export default function Historique() {
+  const [searchActive, setSearchActive] = useState(false)
+
   return (
-    <div>
-      <div className="mt-16 flex items-center justify-center">
-        <Navbar />
-      </div>
+    <div className="mt-14">
+      {!searchActive ? (
+        <div className="absolute left-6 top-11">
+          <Search className="text-title" size={30} onClick={() => setSearchActive(true)} />
+        </div>
+      ) : (
+        <>
+          <div className="absolute left-6 top-9">
+            <MoveLeft
+              color="var(--color-card-tag)"
+              className="w-12 h-12 xl:cursor-pointer"
+              onClick={() => setSearchActive(false)}
+            />
+          </div>
+          <Input name="search" placeholder="Rechercher" type="text" withSearchIcon={true} />
+        </>
+      )}
+      {!searchActive && (
+        <div className="mt-16 flex items-center justify-center">
+          <Navbar />
+        </div>
+      )}
       <div className="mt-14 flex gap-2">
         <ItemCard />
       </div>
