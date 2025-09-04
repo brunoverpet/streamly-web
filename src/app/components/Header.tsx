e97cc0e3fb9611529dd700a448ef424aeb8a1a95
brunoverpet <bverpet46@gmail.com>
2025-09-04 13:58:10 +0200

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="flex justify-end relative">
+    <div className="flex justify-center relative">
       <h1 className="text-title text-5xl font-test">Streamly</h1>
     </div>
   )
