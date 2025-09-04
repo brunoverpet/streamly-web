8c601a1754db346b71987129c953f15428a199c9
brunoverpet <bverpet46@gmail.com>
2025-09-04 09:43:22 +0200

    Update PageTransition.tsx

--- src/app/components/PageTransition.tsx
+++ src/app/components/PageTransition.tsx
@@ -13,7 +13,7 @@ export default function PageTransition({ children }: { children: React.ReactNode
         key={pathname} // déclenche à chaque changement de page
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        exit={{ opacity: 0}}
+        exit={{ opacity: 0 }}
         transition={{ duration: 0.3 }}
       >
         {children}
