cdc99aa22d267388b424a261e0931cc9e0357eaf
brunoverpet <bverpet46@gmail.com>
2025-09-04 13:56:58 +0200

    Update page.tsx

--- src/app/historique/page.tsx
+++ src/app/historique/page.tsx
@@ -2,9 +2,7 @@
 
 import Navbar from '@/app/components/Navbar'
 import ItemCard from '@/app/components/ItemCard'
-import { MoveLeft, Search } from 'lucide-react'
 import { useRef, useState } from 'react'
-import Input from '@/app/components/Input'
 import { useRouter } from 'next/navigation'
 import { motion } from 'motion/react'
 
@@ -30,22 +28,6 @@ export default function Historique() {
         dragDirectionLock={true}
         transition={{ type: 'tween', stiffness: 300, damping: 30 }}
       >
-        {!searchActive ? (
-          <div className="absolute left-6 top-11">
-            <Search className="text-title" size={30} onClick={() => setSearchActive(true)} />
-          </div>
-        ) : (
-          <>
-            <div className="absolute left-6 top-9">
-              <MoveLeft
-                color="var(--color-card-tag)"
-                className="w-12 h-12 xl:cursor-pointer"
-                onClick={() => setSearchActive(false)}
-              />
-            </div>
-            <Input name="search" placeholder="Rechercher" type="text" withSearchIcon={true} />
-          </>
-        )}
         <div className="">
           {!searchActive && (
             <div className="mt-16 flex items-center justify-center">
