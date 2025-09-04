04f3690aca6aa7b5f5b5ea11181ccc90f7d95673
brunoverpet <bverpet46@gmail.com>
2025-09-04 14:44:03 +0200

    Update SeenButton.tsx

--- src/app/components/SeenButton.tsx
+++ src/app/components/SeenButton.tsx
@@ -2,11 +2,17 @@
 
 import { Eye, EyeOff } from 'lucide-react'
 import { useState } from 'react'
+import { toast } from 'sonner'
 
 export default function SeenButton() {
   const [seen, setSeen] = useState(false)
+
+  function handleSeenButton() {
+    setSeen(!seen)
+    toast('Seen button clicked')
+  }
   return (
-    <div onClick={() => setSeen(!seen)}>
+    <div onClick={handleSeenButton}>
       {seen ? <Eye color="white" /> : <EyeOff color="white" />}
     </div>
   )
