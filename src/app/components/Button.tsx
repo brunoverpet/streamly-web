477c1bd7e623e682dfaf3a34c8434c4e66c073ad
brunoverpet <bverpet46@gmail.com>
2025-09-05 12:03:26 +0200

    Update Button.tsx

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,17 +1,14 @@
 'use client'
 
-import { toast } from 'sonner'
-import { ArrowDown } from 'lucide-react'
-
 interface ButtonProps {
   name: string
 }
 export default function Button({ name }: ButtonProps) {
   return (
     <button
-      onClick={() => {
-        toast('This is a toast', { icon: <ArrowDown /> })
-      }}
+      // onClick={() => {
+      //   toast('This is a toast', { icon: <ArrowDown /> })
+      // }}
       className="bg-card-tag p-3 rounded-lg text-title flex items-center w-max"
     >
       {name}
