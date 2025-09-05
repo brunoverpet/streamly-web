5d88ad38e3963791f2c97d535c0c47afb9f90ba3
brunoverpet <bverpet46@gmail.com>
2025-09-05 12:03:31 +0200

    Update Input.tsx

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,22 +1,28 @@
 import { Search } from 'lucide-react'
+import type React from 'react'
 
 interface InputProps {
   label?: string
   name: string
   placeholder: string
   type: 'text' | 'email' | 'password'
+  value?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
   withSearchIcon?: boolean
 }
+
 export default function Input({
   label,
   name,
   placeholder,
   type,
+  value,
+  onChange,
   withSearchIcon = false,
 }: InputProps) {
   return (
     <div className="text-paragraph">
-      <span>{label}</span>
+      {label && <span>{label}</span>}
       <div className="flex items-center gap-x-1 border-paragraph border-1 rounded-sm p-2">
         {withSearchIcon && <Search className="text-title" />}
         <input
@@ -24,6 +30,9 @@ export default function Input({
           name={name}
           placeholder={placeholder}
           type={type}
+          value={value}
+          onChange={onChange}
+          required
           className="w-full outline-none"
         />
       </div>
